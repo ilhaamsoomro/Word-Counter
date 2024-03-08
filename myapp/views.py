ef4a2806a62f8394348e5a8f822d609c85f50388
ilhaamsoomro <ilhaamsoomro@gmail.com>
2024-03-09 01:37:07 +0500

    complete project

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    words = request.GET['text']
-    amount_of_words = len(words.split())
-    return render(request, 'counter.html')
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
